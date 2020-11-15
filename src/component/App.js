7b966da805cd5081cd7eb603aeec02a4d0c44829
ssudipta <phoenix567sid@gmail.com>
2020-11-15 18:30:23 +0530

    Added folder structure

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,6 @@
-import './App.css';
+import '../styles/App.css';
 import React, { useEffect, useState} from 'react';
-import Recipe from './Recipe';
+import Recipe from '../component/Recipe';
 
 
 const App = ()=> {
