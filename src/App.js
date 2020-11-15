a18f67f23dc20e624ac4b461a896bcf711bd3ef5
ssudipta <phoenix567sid@gmail.com>
2020-11-15 13:40:47 +0530

    Basic fetch from recipe API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import React, { useEffect, useState} from 'react';
+import Recipe from './Recipe';
 
-function App() {
+
+const App = ()=> {
+
+  const APP_ID ="9aeeea9b";
+  const APP_KEY = "6004eb32549d76cc639fa924b2260aed";
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes= async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+    setRecipes(data.hits);
+    console.log(recipes);
+  };
+
   return (
     <div className="App">
-      Test
+      <form className="search-form">
+        <input type="text" className="search-input" />
+        <button type="submit" className="search-buttom"> Search </button>
+      </form>
+      {recipes.map( recipe => (
+        <Recipe title={recipe.recipe.label}/>
+      ))}
     </div>
   );
 }
